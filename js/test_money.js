df9f75abb1c95bb995437b600344a933dde2b4e4
cassiobotaro <cassiobotaro@gmail.com>
2022-04-16 17:15:47 -0300

    feat: division and multiplication features done

--- js/test_money.js
+++ js/test_money.js
@@ -1,14 +1,29 @@
 const assert = require('assert');
 
-class Dollar {
-  constructor(amount) {
+class Money {
+  constructor(amount, currency) {
     this.amount = amount;
+    this.currency = currency;
   }
+
   times(multiplier) {
-    return new Dollar(this.amount * multiplier);
+    return new Money(this.amount * multiplier, this.currency);
+  }
+
+  divide(divisor) {
+    return new Money(this.amount / divisor, this.currency);
   }
 }
 
-let fiver = new Dollar(5);
-let tenner = fiver.times(2);
-assert.strictEqual(tenner.amount, 10);
+let fiveDollars = new Money(5, "USD");
+let tenDollars = new Money(10, "USD");
+assert.deepEqual(fiveDollars.times(2), tenDollars);
+
+let tenEuros = new Money(10, "EUR");
+let twentyEuros = new Money(20, "EUR");
+assert.deepEqual(tenEuros.times(2), twentyEuros);
+
+let originalMoney = new Money(4002, "KRW");
+let actualMoneyAfterDivision = originalMoney.divide(4);
+let expectedMoneyAfterDivision = new Money(1000.5, "KRW");
+assert.deepStrictEqual(actualMoneyAfterDivision, expectedMoneyAfterDivision);
